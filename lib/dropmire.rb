de96745fa749bf2dee932e7b792a37e2a7d5d7fd
Connor Jacobsen <jacobsen.connor@gmail.com>
2014-10-06 13:38:36 -0400

    temp require of unused files to fix specs

--- lib/dropmire.rb
+++ lib/dropmire.rb
@@ -2,5 +2,9 @@ require "dropmire/identity"
 require "dropmire/parser"
 require "dropmire/version"
 
+# temporary to fix specs
+require "dropmire/soundex"
+require "dropmire/name_encoder"
+
 module Dropmire
 end
