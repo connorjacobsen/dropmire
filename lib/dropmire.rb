f6715f77c0afadc64472a52d6c1003443ecc9daf
Connor Jacobsen <jacobsen.connor@gmail.com>
2014-10-08 23:11:05 -0400

    remove unused files

--- lib/dropmire.rb
+++ lib/dropmire.rb
@@ -2,9 +2,5 @@ require "dropmire/identity"
 require "dropmire/parser"
 require "dropmire/version"
 
-# temporary to fix specs
-require "dropmire/soundex"
-require "dropmire/name_encoder"
-
 module Dropmire
 end
